d57063f70367af428b57d9e95398d0a46b922613
Kris G <indicoredev@gmail.com>
2012-02-24 15:43:05 -0400

    an updated version with our "report" as the readme

--- libs/kernel.h
+++ libs/kernel.h
@@ -1,11 +1,16 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-//kernel method forward declarations
-struct regs
-#ifndef __SYSTEM_H
-#define __SYSTEM_H
+#define FALSE 0
+#define TRUE 1
 
+#define HEAP 0xD0000
+#define HEAPHW 0xD000000
+#define HEAPMAGIC 0xBABEBEEF
+
+void* currentHeapVal = HEAP;
+
+//kernel method forward declarations
 typedef int size_t;
 
 /* This defines what the stack looks like after an ISR was running */
@@ -17,18 +22,35 @@ struct regs
     unsigned int eip, cs, eflags, useresp, ss;    
 };
 
+typedef struct registers
+{
+    unsigned int ds;
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;
+    unsigned int eip, cs, eflags, useresp, ss;
+}registers_t;
+
+
+typedef struct multiboot_memory_map {
+	unsigned int size;
+	unsigned int base_addr_low,base_addr_high;
+// You can also use: unsigned long long int base_addr; if supported.
+	unsigned int length_low,length_high;
+// You can also use: unsigned long long int length; if supported.
+	unsigned int type;
+} multiboot_memory_map_t;
+
 /* MAIN.C */
 extern void *memcpy(void *dest, const void *src, size_t count);
 extern void *memset(void *dest, char val, size_t count);
 extern unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
 extern size_t strlen(const char *str);
-extern unsigned char inportb (unsigned short _port);
-extern void outportb (unsigned short _port, unsigned char _data);
 
 /* CONSOLE.C */
 extern void init_video(void);
-extern void puts(unsigned char *text);
-extern void putch(unsigned char c);
+extern void println(unsigned char *text);
+extern void print(unsigned char *text);
+extern void printc(unsigned char c);
 extern void cls();
 
 /* GDT.C */
@@ -40,7 +62,7 @@ extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short s
 extern void idt_install();
 
 /* ISRS.C */
-extern void isrs_install();
+extern void isr_install();
 
 /* IRQ.C */
 extern void irq_install_handler(int irq, void (*handler)(struct regs *r));
@@ -48,13 +70,16 @@ extern void irq_uninstall_handler(int irq);
 extern void irq_install();
 
 /* TIMER.C */
-extern void timer_wait(int ticks);
+extern void wait(int ticks);
 extern void timer_install();
 
 /* KEYBOARD.C */
 extern void keyboard_install();
 
-#endif
+/*Memory Managment*/
+extern void* malloc(unsigned size);
+extern void free(void*);
+
 int printGreeting();
 
 #endif
